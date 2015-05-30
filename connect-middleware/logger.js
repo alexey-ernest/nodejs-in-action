e9738c54755d444859954ba400cf00006804571a
Alexey <alexey.ernest@gmail.com>
2015-05-30 19:30:33 +0300

    Logger custom token

--- connect-middleware/logger.js
+++ connect-middleware/logger.js
@@ -1,8 +1,13 @@
 var connect = require('connect');
+var url = require('url');
+
 var logger = require('morgan');
+logger.token('query-string', function (req, res) {
+    return url.parse(req.url).query;
+});
 
 var app = connect()
-    .use(logger('dev'))
+    .use(logger(':method :url :query-string :status :response-time ms'))
     .use(function (req, res, next) {
         res.end('hello\n');
     })
